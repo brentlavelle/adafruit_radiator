b74454c57e31ba377e9df6608f8530107a9ab883
brentlavelle <brentlavelle@yahoo.com>
2016-09-22 20:43:11 -0500

    this class manages a bot for slack

---
+++ lib/slack_monitor.rb
@@ -0,0 +1,31 @@
+require 'slack-ruby-bot'
+
+module AdafruitRadiator
+  class SlackMonitor
+    def initialize token
+      Slack.configure do |config|
+        config.token = token
+      end
+
+      @client = Slack::RealTime::Client.new
+    end
+
+    def color
+      return nil unless @message.attachments
+      @message.attachments.each {
+          |attachment| return attachment.color if attachment.color
+      }
+      return nil
+    end
+
+    def when_message &block
+      @client.on :message do |data|
+        yield data
+      end
+    end
+
+    def start
+      @client.start!
+    end
+  end
+end
